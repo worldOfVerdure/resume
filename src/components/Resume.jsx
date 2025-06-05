fb96b896ad8d952a59e16e66d7d8f652acf7c352
worldOfVerdure <perspicaciousdotard@gmail.com>
2025-06-04 22:16:10 -0400

    Initial set up of project

---
+++ src/components/Resume.jsx
@@ -0,0 +1,22 @@
+import ResumeHeader from "./ResumeHeader/ResumeHeader.jsx";
+import { styled } from "styled-components";
+
+export default function Resume() {
+  return (
+    <ResumePage >
+      <ResumeHeader />
+    </ResumePage>
+  );
+}
+
+
+const ResumePage = styled.div`
+  border: .2rem solid #000;
+  height: 11in;
+  margin: 0 auto;
+  width: 8.5in;
+
+  & h1 {
+    margin: 0;
+  }
+`;
